b39c3fce1b2bb5784b09e0df039442c9208e6134
Ashish Shevale <shevaleashish@gmail.com>
2022-07-20 22:08:48 +0530

    Create the Dialog component as per tests

---
+++ components/Dialog/index.tsx
@@ -0,0 +1,34 @@
+import React, { MouseEventHandler } from "react";
+
+export interface Props {
+    isOpen: boolean,
+    closeDialog: MouseEventHandler<HTMLButtonElement | HTMLDivElement>,
+}
+
+function Dialog(props: Props): JSX.Element {
+    const dialogVisibility = props.isOpen ? "visible" : "hidden";
+
+    return (
+        <div role="dialog" style={{visibility: dialogVisibility}}>
+            <div 
+                data-testid="dialog-backdrop-testId"
+                style={{width: "100vw", height: "100vh"}}
+            >
+            </div>
+
+            <div 
+                data-testid="dialog-content-testId"
+                style={{zIndex: "100"}}
+            >
+                <button 
+                    type="button"
+                    role="button" 
+                    data-testid="dialog-close-testId"
+                    onClick={props.closeDialog}
+                ></button>
+            </div>
+        </div>
+    );
+}
+
+export default Dialog;
