efb980ad27067bbe917121ab0ca621613eb9e32a
Ashish Shevale <shevaleashish@gmail.com>
2022-07-24 14:10:14 +0530

    Implement basic ElementContainer with all form element names

--- features/ElementContainer/index.tsx
+++ features/ElementContainer/index.tsx
@@ -1,9 +1,19 @@
 import React from "react";
+import BooleanDisplayName from "../FormElements/BooleanElement/DisplayName";
+import InputDisplayName from "../FormElements/InputElement/DisplayName";
+import CheckboxDisplayName from "../FormElements/CheckboxElement/DisplayName";
+import DropdownDisplayName from "../FormElements/DropdownElement/DisplayName";
+import RadioDisplayName from "../FormElements/RadioButtonElement/DisplayName";
 
 function ElementContainer() {
     return (
-        <>
-        </>
+        <div>
+            <BooleanDisplayName />
+            <InputDisplayName />
+            <CheckboxDisplayName />
+            <DropdownDisplayName />
+            <RadioDisplayName />
+        </div>
     );
 }
 
